6162b2a96abd049ae46dd0ebb8539ee6346094ab
Kenneth <kennethjnvega@gmail.com>
2024-11-11 04:02:39 -0600

    Delete Solution1 directory

--- Solution1/Project1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Project1;
-using System.ComponentModel.DataAnnotations;
-
-Book book = new Book("The lord of the rings","Frodo",1990,1000);
-Book book2 = new Book("El evangelio del mal","Lewis");
-
-Console.WriteLine();
